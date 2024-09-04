d6a8b784ee5f1adf39ec9e0b8a2149f13ac96e21
MFrangi <manuel.frangi@ign.fr>
2024-09-04 08:43:55 +0000

    maj tests ter

--- test/controllers/gpu/municipality.js
+++ test/controllers/gpu/municipality.js
@@ -18,6 +18,21 @@ describe('with insee=25349', function() {
     });
 });
 
+describe('with point at [1.654399,48.112235] (Rennes)', function() {
+    it('should reply a FeatureCollection containing a valid Feature', function(done) {
+        request(app)
+            .get('/api/gpu/municipality?geom={"type":"Point","coordinates":[1.654399,48.112235]}')
+            .expect(200)
+            .expect(res => {
+                expect(res.body.features.length).to.eql(1);
+                const feature = res.body.features[0];
+                expect(feature.properties.is_rnu).to.eql(false);
+            })
+            .end(done);
+        ;
+    });
+});
+
 describe('/api/gpu/municipality', function() {
     describe('without filtering parameter', function() {
         it('should reply with 400', function(done) {
@@ -31,18 +46,4 @@ describe('/api/gpu/municipality', function() {
         });
     });
 
-    describe('with point at [1.654399,48.112235] (Rennes)', function() {
-        it('should reply a FeatureCollection containing a valid Feature', function(done) {
-            request(app)
-                .get('/api/gpu/municipality?geom={"type":"Point","coordinates":[1.654399,48.112235]}')
-                .expect(200)
-                .expect(res => {
-                    expect(res.body.features.length).to.eql(1);
-                    const feature = res.body.features[0];
-                    expect(feature.properties.is_rnu).to.eql(false);
-                })
-                .end(done);
-            ;
-        });
-    });
 });
