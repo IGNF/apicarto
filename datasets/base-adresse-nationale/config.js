1b804a2896f8a991190a5a04969d69d019ad4665
Manuel Frangi <manuel.frangi@ign.fr>
2025-03-12 13:48:09 +0000

    maj fichiers yml au format openapi 3.1.0

--- datasets/base-adresse-nationale/config.js
+++ datasets/base-adresse-nationale/config.js
@@ -1,5 +1,5 @@
 var banConfig = {
-    version: 'v4.1.1',
+    version: 'v4.1.2',
     modules: ["Codes Postaux"],
     nom_url :"BAN",
     url: 'https://github.com/baseadressenationale/codes-postaux'
